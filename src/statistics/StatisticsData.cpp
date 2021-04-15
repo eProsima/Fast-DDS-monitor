dc857c2b11a6d4f977471850917554ec49d910bc
jparisu <javierparis@eprosima.com>
2021-04-15 11:22:08 +0200

    Refs #11202: license and headers sort in .cpp
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/statistics/StatisticsData.cpp
+++ src/statistics/StatisticsData.cpp
@@ -1,16 +1,30 @@
-#include <include/statistics/StatisticsData.h>
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include <QtCharts/QXYSeries>
+
+#include <QDateTime>
+#include <QDebug>
 #include <QtCharts/QAreaSeries>
-#include <QtQuick/QQuickView>
-#include <QtQuick/QQuickItem>
+#include <QtCharts/QCategoryAxis>
+#include <QtCharts/QDateTimeAxis>
+#include <QtCharts/QXYSeries>
 #include <QtCore/QDebug>
 #include <QtCore/QRandomGenerator>
-#include <QtCharts/QDateTimeAxis>
-#include <QtCharts/QCategoryAxis>
-#include <QDateTime>
+#include <QtQuick/QQuickView>
+#include <QtQuick/QQuickItem>
 
-#include <QDebug>
+#include <include/statistics/StatisticsData.h>
 
 QT_CHARTS_USE_NAMESPACE
 
