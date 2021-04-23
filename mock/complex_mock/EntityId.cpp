8cad96fcacf71143b0728781c582c6551e623292
jparisu <javierparis@eprosima.com>
2021-04-23 13:01:05 +0200

    Refs #11300: add noexcept
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- mock/complex_mock/EntityId.cpp
+++ mock/complex_mock/EntityId.cpp
@@ -12,46 +12,52 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <fastdds-statistics-backend/types/EntityId.hpp>
+/**
+ * @file EntityId.cpp
+ */
 
-#include "database/Database.hpp"
+#include <fastdds-statistics-backend/types/EntityId.hpp>
 
 namespace eprosima {
 namespace statistics_backend {
 
-// Mock up the EntityId default constructor to auto increment value
-EntityId::EntityId()
+/*
+ * Standar EntityId
+ * No modifications made over the std EntityId implementation
+ */
+
+EntityId::EntityId() noexcept
     : value_(ENTITY_ID_INVALID)
 {
 }
 
 EntityId::EntityId(
-        int64_t value)
+        int64_t value) noexcept
     : value_(value)
 {
 }
 
-EntityId EntityId::all()
+EntityId EntityId::all() noexcept
 {
     return EntityId(ENTITY_ID_ALL);
 }
 
-EntityId EntityId::invalid()
+EntityId EntityId::invalid() noexcept
 {
     return EntityId(ENTITY_ID_INVALID);
 }
 
-bool EntityId::is_valid()
+bool EntityId::is_valid() noexcept
 {
     return (value_ >= 0);
 }
 
-void EntityId::invalidate()
+void EntityId::invalidate() noexcept
 {
     value_ = ENTITY_ID_INVALID;
 }
 
-int64_t EntityId::value() const
+int64_t EntityId::value() const noexcept
 {
     return value_;
 }
