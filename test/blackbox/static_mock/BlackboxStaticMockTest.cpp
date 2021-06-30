8593c9e54063bb30f839937bc65e9bf3d353193d
RaulSanchez <raul@eprosima.com>
2021-06-30 09:18:11 +0200

    Added gtest to blackbox tests
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- test/blackbox/static_mock/BlackboxStaticMockTest.cpp
+++ test/blackbox/static_mock/BlackboxStaticMockTest.cpp
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "gtest/gtest.h"
+#include <gtest_aux.hpp>
+#include <gtest/gtest.h>
 
 #include <QApplication>
 
