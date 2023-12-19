b135360f531b4e2a3946c1ec64f5156e93558f97
JesusPoderoso <jesuspoderoso@eprosima.com>
2023-12-19 11:47:27 +0100

    Refs #19865: Init monitor with proper app id
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- src/backend/SyncBackendConnection.cpp
+++ src/backend/SyncBackendConnection.cpp
@@ -442,7 +442,7 @@ EntityId SyncBackendConnection::init_monitor(
 {
     try
     {
-        return StatisticsBackend::init_monitor(domain);
+        return StatisticsBackend::init_monitor(domain, nullptr, CallbackMask::all(), DataKindMask::none(), FASTDDS_MONITOR_APP);
     }
     catch (const Error& e)
     {
@@ -464,7 +464,8 @@ EntityId SyncBackendConnection::init_monitor(
     {
         return StatisticsBackend::init_monitor(
             discovery_server_guid_prefix,
-            discovery_server_locators);
+            discovery_server_locators,
+            nullptr, CallbackMask::all(), DataKindMask::none(), FASTDDS_MONITOR_APP);
     }
     catch (const Error& e)
     {
