78133a70860c3cf966dba8911d7d693eb3587312
Juanjo Garcia <juanjosegarcia@eprosima.com>
2025-01-21 07:52:17 +0100

    Refs #22575: Updated side logo
    Signed-off-by: Juanjo Garcia <juanjosegarcia@eprosima.com>

--- docs/conf.py
+++ docs/conf.py
@@ -275,7 +275,7 @@ html_theme_options = {
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 
-html_logo = 'rst/_static/css/imgs/fastdds_monitor_alt_side_logo.png'
+html_logo = 'rst/_static/css/imgs/fastdds_monitor_new_logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
